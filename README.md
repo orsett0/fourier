3058ee3c7df4925c787023514e6d26b318f1d646
Alessio Orsini <35498861+orsett0@users.noreply.github.com>
2023-04-05 00:12:07 +0200

    Code's cleaner now.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Fourier calculator
+
+Raccolta (prima o poi) di script per plottare i grafici delle serie/trasformate di fourier, con rispettivi spettri di ampiezza e fase.
--- series.py
+++ series.py
@@ -2,50 +2,55 @@
 
 import matplotlib.pyplot as plt
 import numpy
-from scipy import signal
-from scipy.integrate import quad
+from scipy import signal, integrate
 
 w = 1
 T_wdt = (2 * numpy.pi) / w
 T = (0, T_wdt)
-N = list(range(-50, 51))
+resolution = 100
+N = list(range(-resolution // 2, resolution // 2 + 1))
 
 func = signal.sawtooth
 
+print(f"calculating real function...")
 x = numpy.arange(-T_wdt * 2, T_wdt * 2, 0.1)
 y = [func(t) for t in x]
 
+# This takes a lot of time. Maybe i can find a way to speed things up a bit.
+# Compute the integral splitting a complex number by it's real and imaginary parts
 def complex_quadrature(func, a, b):
     def real_func(x):
         return numpy.real(func(x))
     def imag_func(x):
         return numpy.imag(func(x))
     
-    real_integral = quad(real_func, a, b)
-    imag_integral = quad(imag_func, a, b)
+    real_integral = integrate.quad(real_func, a, b, limit=resolution)
+    imag_integral = integrate.quad(imag_func, a, b, limit=resolution)
 
     return real_integral[0] + 1j*imag_integral[0]
 
+print(f"calculating coefficients...")
 coeff = [complex_quadrature(lambda t: func(t) * numpy.exp(- 1j * n * w * t), T[0], T[1]) / T_wdt for n in N]
-rstrd = [sum([coeff[n + numpy.abs(N[0])] * numpy.exp(1j * w * n * t) for n in N]) for t in x]
-
 mods = [numpy.abs(e) for e in coeff]
 args = [numpy.angle(e) for e in coeff]
+
+print(f"calculating restored function...")
+rstrd = [sum([coeff[n + numpy.abs(N[0])] * numpy.exp(1j * w * n * t) for n in N]) for t in x]
 
 plt.subplot(3, 1, 1)
 plt.title("Spettro di ampiezza")
 plt.ylabel("module")
 plt.xlabel("freq")
+plt.vlines(N, ymin=0, ymax=[ a if a > 0 else 0 for a in mods ], colors='grey', linestyles='solid')
 plt.plot(N, mods, '.', color='black')
-plt.vlines(N, ymin=0, ymax=[ a if a > 0 else 0 for a in mods ], colors='black', linestyles='solid')
 plt.axhline(y=0, color='black')
 
 plt.subplot(3, 1, 2)
 plt.title("Spettro di fase")
 plt.ylabel("arg")
 plt.xlabel("freq")
+plt.vlines(N, ymin=[ a if a < 0 else 0 for a in args ], ymax=[ a if a > 0 else 0 for a in args ], colors='grey', linestyles='solid')
 plt.plot(N, args, '.', color='black')
-plt.vlines(N, ymin=[ a if a < 0 else 0 for a in args ], ymax=[ a if a > 0 else 0 for a in args ], colors='black', linestyles='solid')
 plt.axhline(y=0, color='black')
 
 plt.subplot(3, 1, 3)
